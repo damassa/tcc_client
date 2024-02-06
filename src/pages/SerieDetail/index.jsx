d51d39e898e8179c324a995affa1f0261eb12a25
Damassa <felipelealdamasceno@gmail.com>
2024-02-06 18:51:26 -0300

    Serie detail updated

--- src/pages/SerieDetail/index.jsx
+++ src/pages/SerieDetail/index.jsx
@@ -10,16 +10,23 @@ const SerieDetail = () => {
                 <div className="detail--image--container">
                     <img src={PosterImage} alt="" />
                 </div>
-                <div className="detail--info">
+                <div className="detail--info--name">
                     <h1>Dengeki Sentai Changeman</h1>
-                    <AddFavoriteButton />
+                    <h2>1985</h2>
+                    <div className="detail--favorite">
+                        <AddFavoriteButton />
+                    </div>
+                </div>
+                <div className="detail--plot">
+                    <p>
+                        Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Negão é teu passadis, eu sou faxa pretis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Detraxit consequat et quo num tendi nada.
+                        Si num tem leite então bota uma pinga aí cumpadi! Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Pra lá, depois divoltis porris, paradis.
+                    </p>
+                </div>
+                <div className="detail--info">
+                    lalala
                 </div>
             </div>
-            <p className="detail--plot">
-            Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Negão é teu passadis, eu sou faxa pretis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Detraxit consequat et quo num tendi nada.
-
-Si num tem leite então bota uma pinga aí cumpadi! Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Pra lá, depois divoltis porris, paradis.
-            </p>
         </section>
     )
 }
